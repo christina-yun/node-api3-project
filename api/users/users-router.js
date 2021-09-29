8eea09a153914765336fb71185479b7b041cbc5f
christina-yun <poncheeyun@yahoo.com>
2021-09-29 15:56:45 -0700

    Finished router.put /:id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+
 const { validateUserId, validateUser, validatePost } = require('./../middleware/middleware'); 
 const Users = require('./users-model');
 const Posts = require('./../posts/posts-model');
@@ -32,13 +31,18 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(next)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
